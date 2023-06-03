f7f11a622527a6b7434080a0c632d71b3c795871
Chirag Chamoli <chirax@gmail.com>
2023-06-03 16:47:03 +0530

    Update api.py

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -5,9 +5,9 @@ from app.users.urls import user_router
 from app.group.urls import group_router
 router = APIRouter()
 
-router.include_router(payment_router, tags=["Payments"], prefix="/payments")
 router.include_router(auth_router, tags=["Auth"], prefix="/auth")
 router.include_router(user_router, tags=["Users"], prefix="/users")
 router.include_router(group_router, tags=["Groups"], prefix="/groups")
+router.include_router(payment_router, tags=["Payments"], prefix="/payments")
 
 
