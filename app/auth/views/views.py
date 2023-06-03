83e421e842fd7d1bb311d4383d4f009ba7dd3f43
Chirag Chamoli <chirax@gmail.com>
2023-06-03 12:26:26 +0530

    Routes

--- app/auth/views/views.py
+++ app/auth/views/views.py
@@ -1,7 +1,37 @@
-from fastapi import APIRouter, Cookie, Request
+from typing import Union
+from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/", name="Root")
-def index():
-    pass
+@router.post("/signup", name="Signup")
+def signup():
+    """
+    Signs up a new user.
+
+    Returns:
+        str: Message indicating successful user signup.
+    """
+    return "Signup User"
+
+@router.post("/login", name="Login")
+def login():
+    """
+    Logs in a user.
+
+    Returns:
+        str: Message indicating successful user login.
+    """
+    return "Login User"
+
+@router.post("/logout/{user_id}", name="Logout")
+def logout(user_id: int):
+    """
+    Logs out a user.
+
+    Args:
+        user_id (int): ID of the user.
+
+    Returns:
+        str: Message indicating successful user logout.
+    """
+    return "Logout"
--- app/payments/views/views.py
+++ app/payments/views/views.py
@@ -1,7 +1,60 @@
-from fastapi import APIRouter, Cookie, Request
+from typing import Union
+from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/", name="Root")
-def index():
-    pass
+@router.get("/{user_id}/history", name="Payments History")
+def payments_history(user_id: int):
+    """
+    Retrieves the payment history for a user.
+
+    Args:
+        user_id (int): ID of the user.
+
+    Returns:
+        str: Message indicating successful retrieval of payment history.
+    """
+    return "Payments History"
+
+
+@router.get("/{user_id}/refund/{payment_instance_id}", name="Payments Instance Refund")
+def payment_refund(user_id: int, payment_instance_id: str):
+    """
+    Refunds a specific payment instance for a user.
+
+    Args:
+        user_id (int): ID of the user.
+        payment_instance_id (str): ID of the payment instance.
+
+    Returns:
+        str: Message indicating successful refund of a payment instance.
+    """
+    return "Payments Refund"
+
+
+@router.get("/{user_id}/create_checkout_session", name="Create Checkout Session")
+def create_checkout_session(user_id: int):
+    """
+    Creates a checkout session for a user.
+
+    Args:
+        user_id (int): ID of the user.
+
+    Returns:
+        str: Message indicating successful creation of a checkout session.
+    """
+    return "Create Checkout Session"
+
+
+@router.get("/{user_id}/customer_portal_session", name="Customer Portal Session")
+def customer_portal_session(user_id: int):
+    """
+    Retrieves the customer portal session for a user.
+
+    Args:
+        user_id (int): ID of the user.
+
+    Returns:
+        str: Message indicating successful retrieval of the customer portal session.
+    """
+    return "Customer Portal Session"
